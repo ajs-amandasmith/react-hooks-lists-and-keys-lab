182496aa8198c13b67147e0bc3fdda0f2258996c
Amanda Smith <ajs.amandasmith@gmail.com>
2022-05-21 21:11:08 -0500

    pass the tests

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const techMap = technologies.map(tech => {
+    return <span key={tech}>{tech}</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+        {techMap}
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
